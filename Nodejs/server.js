07ba59fe0d1639343609f29e7dda16a543474bf3
kimbuss8877 <98803862+kimbuss8877@users.noreply.github.com>
2025-11-07 22:00:46 +0900

    251107
    2201

---
+++ Nodejs/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 80;
+
+// body-parser 미들웨어 설정
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// 로그인 페이지를 렌더링하는 라우트
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '.login.html'); // 로그인 폼 HTML 파일을 전송
+});
+
+// 로그인 폼에서 POST 요청이 올 때 처리하는 라우트
+app.post('/login', (req, res) => {
+    const user_id = req.body.user_id;
+    const password = req.body.password; 
+    var sql = 'select * from member where user_id=? and password=?'
+    
+    var values = [user_id,password];
+    // 실제 서비스에서는 데이터베이스와 연동하여 아이디/비밀번호를 검증합니다.
+    // 여기서는 간단하게 콘솔 로그를 출력합니다.
+    console.log(`아이디: ${user_id}, 비밀번호: ${password}`);
+
+    connection.query(sql, values, function(err, result){
+        if(err) throw err;
+        res.send(result);
+    })
+});
+ 
+
+// 서버 시작
+app.listen(port, () => {
+    console.log(`서버가 http://gamex.iptime.org:${port} 에서 실행 중입니다.`);
+});
+
